6581242140c51b52916fca03a296b4a9edc961ff
GEANT <maximelagrange98@gmail.com>
2024-09-19 16:07:26 +0200

    modified plotting parameters

--- muograph/plotting/params.py
+++ muograph/plotting/params.py
@@ -1,3 +1,32 @@
-n_bins = 50
-alpha = 0.6
-d_unit = "mm"
+import math
+from typing import Tuple
+
+# Number of bins for histograms
+n_bins: int = 50
+
+# Histogram transparency
+alpha: float = 0.6
+
+# Distance unit
+d_unit: str = "mm"
+
+# Matplotlib default font
+fontsize: int = 13
+labelsize: int = 12
+titlesize: int = 16
+fontweigh: str = "normal"
+font = {"weight": "normal", "size": fontsize, "family": "Arial"}
+
+# Matplotlib colormaps
+cmap: str = "jet"
+
+# Histogram XY ratio
+xy_golden_ratio: float = (1 + math.sqrt(5)) / 2
+hist_scale = 4.5
+hist_figsize: Tuple[float, float] = (xy_golden_ratio * hist_scale, 1 * hist_scale)
+
+
+# Scale of matplotlib figures with subplots
+# Size of figures with subplots is defined as
+# (scale * ncols * xy_ratio[0], scale * nrows * xy_ratio[1])
+scale: int = 3
