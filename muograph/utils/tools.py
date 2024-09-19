5eaf47eae6a7156b78a0246c9167186142c81c14
GEANT <maximelagrange98@gmail.com>
2024-09-19 16:15:42 +0200

    add gaussian filter and normalize methods

--- muograph/utils/tools.py
+++ muograph/utils/tools.py
@@ -5,27 +5,39 @@ import numpy as np
 from typing import Union, List
 
 
-def normalize(s: Tensor) -> Tensor:
-    s_max, s_min = torch.max(s), torch.min(s)
+def normalize(x: Union[Tensor, np.ndarray]) -> Union[Tensor, np.ndarray]:
+    r"""Normalize the input array.
 
-    return (s - s_min) / (s_max - s_min)
+    Args:
+        x (Union[Tensor, np.ndarray]): Input array.
+
+    Returns:
+        array (Union [Tensor, np.ndarray]): The normalized array.
+    """
+    if isinstance(x, Tensor):
+        return (x - torch.min(x)) / (torch.max(x) - torch.min(x))
+    elif isinstance(x, np.ndarray):
+        return (x - np.min(x)) / (np.max(x) - np.min(x))
+    else:
+        raise TypeError(
+            f"Input type {type(x)} is not supported. Expected Tensor or np.ndarray."
+        )
 
 
 def apply_gaussian_filter(
     x: Union[Tensor, np.ndarray], sigma: List[float]
 ) -> Union[Tensor, np.ndarray]:
-    """
-    Applies a Guassian filter to the provided array.
-    (https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.gaussian_filter.html)
+    r"""Applies a Guassian filter to the provided array:
+    (https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.gaussian_filter.html).
 
     Args:
-        x (Union[Tensor, np.dnarray]): The input array.
-        sigma (Union[float, Tuple[float, ...]]): Standard deviation for Gaussian kernel.
-        The standard deviations of the Gaussian filter are given for each axis as a sequence,
+        x (Union[Tensor, np.dnarray]): Input array.
+
+        sigma (Union[float, Tuple[float, ...]]): Standard deviation for Gaussian kernel.The standard deviations of the Gaussian filter are given for each axis as a sequence,
         or as a single number, in which case it is equal for all axes.
 
     Returns:
-        Union[Tensor, np.ndarray]: Returned array of same shape as input.
+        array (Union[Tensor, np.ndarray]): Returned array of same shape as input.
     """
 
     if isinstance(x, Tensor):
@@ -33,9 +45,13 @@ def apply_gaussian_filter(
     elif isinstance(x, np.ndarray):
         ndim = len(x.shape)
 
-    if (ndim != len(sigma)) & (len(sigma) > 1):
-        raise ValueError(
-            "`sigma` should have one element for each dimension of `x`, or have a single element."
-        )
-    else:
+    if ndim != len(sigma):
+        raise ValueError("`sigma` must have one element for each dimension of `x`.")
+    elif isinstance(x, Tensor):
+        return torch.tensor(gaussian_filter(x.numpy(), sigma=sigma))
+    elif isinstance(x, np.ndarray):
         return gaussian_filter(x, sigma=sigma)
+    else:
+        raise TypeError(
+            f"Input type {type(x)} is not supported. Expected Tensor or np.ndarray."
+        )
