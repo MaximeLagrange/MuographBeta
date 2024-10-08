83f573958df0e4d6854f09db073b0503724be34d
GEANT <maximelagrange98@gmail.com>
2024-10-08 09:46:53 +0200

    added pre-setting for #bins of 2D histograms

--- muograph/plotting/params.py
+++ muograph/plotting/params.py
@@ -24,8 +24,10 @@ xy_golden_ratio: float = 1.4
 hist_scale = 4.5
 hist_figsize: Tuple[float, float] = (xy_golden_ratio * hist_scale, 1 * hist_scale)
 
-
 # Scale of matplotlib figures with subplots
 # Size of figures with subplots is defined as
 # (scale * ncols * xy_ratio[0], scale * nrows * xy_ratio[1])
 scale: int = 3
+
+# 2D histogram
+n_bins_2D = 100
